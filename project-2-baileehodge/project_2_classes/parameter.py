aca8f2678e986cb455680f615829896a1ee8a800
Brady Hodge <bradyhodge1@gmail.com>
2023-10-18 09:28:41 -0600

    test lol

--- project-2-baileehodge/project_2_classes/parameter.py
+++
@@ -1,7 +0,0 @@
-class Parameter():
-    def __init__(self, value: str, is_id: bool):
-        self.value = value
-        self.is_id = is_id
-
-    def to_string(self):
-        return(f'{self.value}')
--- project-2-baileehodge/project_2_classes/rule.py
+++
@@ -1,9 +0,0 @@
-from predicate import Predicate
-
-class Rule():
-    def __init__(self, head: Predicate, body: list[Predicate]):
-        self.head_predicate = head
-        self.body_predicates = body
-
-    def to_string(self) -> str:
-        return(f'{self.to_string()}')
